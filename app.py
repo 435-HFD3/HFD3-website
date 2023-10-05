e6fe971f4635c40a7be80eea3bec012337559133
andrewdag <andrewdag12@gmail.com>
2023-10-04 20:59:28 -0400

    host change

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ from flask_app import create_app
 
 app  = create_app(debug = True)
 if __name__ == "__main__":
-	app.run(host='cse.msu.edu', port=int(os.environ.get("PORT", 8080)), use_reloader=True)
+	app.run(host='35.9.20.20', port=int(os.environ.get("PORT", 8080)), use_reloader=True)
 	#socketio.run(app, host='0.0.0.0', port=int(os.environ.get("PORT", 8080)))
 	#app.run()
